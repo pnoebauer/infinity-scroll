afa99893cd671b6a351779d46ea4e1bc57d853e6
Philip Noebauer <pnoebauer>
2020-11-27 10:39:35 +0800

    start on logic for infinity scroll

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function setAttributes(element, attributes) {
     for(const key in attributes) {
         // Set key and value of attribute
         element.setAttribute(key, attributes[key]);
-        // console.log(key, attributes[key]);
     }
 }
 
@@ -50,6 +49,25 @@ async function getPhotos() {
         console.log(error);
     }
 }
+
+// Check to see if scrolling near bottom of page occurs and then load more photos
+window.addEventListener('scroll', () => {
+    // console.log('innerHeight',window.innerHeight); //total height of browser window (always the same)
+    // console.log('scrollY',window.scrollY); //distance from top of window user has scrolled
+    // console.log('offsetHeight',document.body.offsetHeight); //height of everything in the body, including what is not within view (here: all images; value can change, e.g. when more pictures are loaded)
+
+    // Load when the scroll is 1000px from the bottom of the content
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+        console.log('innerHeight',window.innerHeight); //total height of browser window (always the same)
+        console.log('scrollY',window.scrollY); //distance from top of window user has scrolled
+        console.log('offsetHeight',document.body.offsetHeight); //height of everything in the body, including what is not within view (here: all images; value can change, e.g. when more pictures are loaded)
+
+        console.log('innerHeight + scrollY',window.innerHeight+window.scrollY); //equal to bottom of page of current scroll (scrollY refers to the top)
+        console.log('offsetHeight - 1000',document.body.offsetHeight - 1000); //1000px from the bottom of all the content
+
+        console.log('load more');
+    }
+})
 
 //On load
 getPhotos();
